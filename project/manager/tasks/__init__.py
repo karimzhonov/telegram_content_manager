b901cfbeb4e672c51c0ef1aae3fd35673ea9c6b6
Khusniddin <khtkarimzhonov@gmail.com>
2022-05-22 11:17:11 +0300

    0.0.2 Post support

--- project/manager/tasks/__init__.py
+++ project/manager/tasks/__init__.py
@@ -6,7 +6,7 @@ aioschedule.every().day.at('7:00').do(task_send_content)
 aioschedule.every().day.at('11:00').do(task_send_content)
 aioschedule.every().day.at('16:00').do(task_send_content)
 
-# aioschedule.every(10).seconds.do(task_send_content)
+aioschedule.every(60).seconds.do(task_send_content)
 
 async def start():
     while True:
