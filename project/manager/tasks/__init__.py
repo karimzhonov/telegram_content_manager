86b333ac20e1c49aaeecbf41747bb4170eb43049
Khusniddin <khtkarimzhonov@gmail.com>
2022-05-22 11:23:14 +0300

    0.0.2 Post support

--- project/manager/tasks/__init__.py
+++ project/manager/tasks/__init__.py
@@ -2,11 +2,12 @@ import asyncio
 import aioschedule
 from .task_send_content import task_send_content
 
-aioschedule.every().day.at('7:00').do(task_send_content)
-aioschedule.every().day.at('11:00').do(task_send_content)
-aioschedule.every().day.at('16:00').do(task_send_content)
+aioschedule.every().day.at('1:00').do(task_send_content)  # +8:00
+aioschedule.every().day.at('5:00').do(task_send_content)
+aioschedule.every().day.at('12:00').do(task_send_content)
 
-aioschedule.every(60).seconds.do(task_send_content)
+
+# aioschedule.every(10).seconds.do(task_send_content)
 
 async def start():
     while True:
