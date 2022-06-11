9401470ef3a5118ee4c6e8e48e0656b59f4b8d0d
Khusniddin <khtkarimzhonov@gmail.com>
2022-06-11 19:59:32 +0300

    Set schedule

--- project/manager/tasks/__init__.py
+++ project/manager/tasks/__init__.py
@@ -3,8 +3,6 @@ import aioschedule
 from .task_send_content import task_send_content
 
 aioschedule.every().day.at('7:00').do(task_send_content)
-aioschedule.every().day.at('11:00').do(task_send_content)
-aioschedule.every().day.at('16:00').do(task_send_content)
 
 # aioschedule.every(10).seconds.do(task_send_content)
 
